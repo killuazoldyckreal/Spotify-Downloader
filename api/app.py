b68c0c5d1872b272204f7a2e04a2a3a6b2026fac
Killua Zoldyck <58257876+killuazoldyckreal@users.noreply.github.com>
2024-01-16 14:53:09 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, send_file, redirect, jsonify,
 import logging, os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.cache_handler import CacheHandler
 from api.helper import is_valid_spotify_url
 import traceback
 from flask_cors import CORS
