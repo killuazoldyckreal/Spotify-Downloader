6524a28a96d544744e5352a05db22ee98ff27354
Killua Zoldyck <58257876+killuazoldyckreal@users.noreply.github.com>
2024-01-16 10:15:55 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,14 +1,10 @@
-from gevent import monkey
-monkey.patch_all()
 from flask import Flask, request, render_template, send_file, redirect, jsonify, after_this_request, send_from_directory
-from flask_socketio import SocketIO
-import logging, os, yt_dlp
+import logging, os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from api.chandler import CustomCacheHandler
 from api.helper import MDATA, is_valid_spotify_url, get_song_metadata, estimate_conversion_time
 import traceback
-import glob
 from flask_cors import CORS
 from gevent.pywsgi import WSGIServer
 from flask_compress import Compress
@@ -24,7 +20,6 @@ compress = Compress()
 compress.init_app(app)
 
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, secure=True, cors_allowed_origins="*", async_mode='gevent')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 cache_path = "api/.spotify_cache"
@@ -41,37 +36,33 @@ def verification_file(filename):
 
 @app.route('/')
 def home():
-    if 'urlInput' in request.args:
-        url = request.args['urlInput']
-        if url=="":
-            return jsonify({'success': False, 'error': 'Song field cannot be empty'}), 400
-        if is_valid_spotify_url(url):
-            if "track" in url:
-                track_id = url.replace("https://open.spotify.com/track/","").split("?")[0]
-                try:
-                    item = {"track" : sp.track(track_id)}
-                    _id = item['track']['id']
-                    track_info = sp.track(_id)
-                    song_title = track_info['name']
-                    data = get_song_metadata(track_info, sp)
-                    path = get_mp3(data,"sp")
-                except:
-                    traceback.print_exc()
-                    return jsonify({'success': False, 'error': 'Song not found or invalid URL'}), 400
+    return render_template('home.html')
+
+@app.route('/download', methods=['POST'])
+def home():
+    if request.is_json:
+        data = request.get_json()
+        if 'track_id' in data:
+            track_id = data.get('track_id')
+            url = 'https://api.spotifydown.com/download/' + track_id
+            try:
+                track_info = sp.track(_id)
+                song_title = track_info['name']
+                data = get_song_metadata(track_info, sp)
+                path = get_mp3(data,url)
+            except:
+                traceback.print_exc()
+                return jsonify({'success': False, 'error': 'Song not found or invalid URL'}), 400
         else:
-            if "https://" not in url:
-                song_title = url
-                try:
-                    try:
-                        results = sp.search(q=song_title, type='track', limit=1)['tracks']['items'][0]
-                    except:
-                        socketio.emit('error_', {'error': 'Song not found'})
-                        return jsonify({'success': False, 'error': 'Song not found'}), 400
-                    data = get_song_metadata(results, sp)
-                    song_title = data['track_name']
-                    path = get_mp3(data,"sp")
-                except:
-                    return jsonify({'success': False, 'error': 'Song not found'}), 400
+            track_name = data.get('name')
+            try:
+                results = sp.search(q=song_title, type='track', limit=1)['tracks']['items'][0]
+            except:
+                return jsonify({'success': False, 'error': 'Song not found'}), 400
+            data = get_song_metadata(results, sp)
+            url = 'https://api.spotifydown.com/download/' + data['_id']
+            song_title = data['track_name']
+            path = get_mp3(data,url)
 
         try:
             file_handle = open(path, 'r')
@@ -83,12 +74,11 @@ def home():
                 except Exception as error:
                     app.logger.error("Error removing or closing downloaded file handle", error)
                 return response
-            socketio.emit('converted', {'status':'finished'})
-            return send_file(path, download_name = song_title + '.mp3', as_attachment=True, mimetype='audio/mpeg'), 200
+            return send_file(path, download_name = path, as_attachment=True, mimetype='audio/mpeg'), 200
         except Exception as e:
             return jsonify({'success': False, 'error': str(e)}), 400
     else:
-        return render_template('home.html', async_mode=socketio.async_mode)
+        return render_template('home.html')
 
 def progress_hook(d):
     if d['status'] == 'downloading':
@@ -97,37 +87,54 @@ def progress_hook(d):
         socketio.emit('finished', {'status':'finished'})
 
 def get_mp3(data, query):
-    squery = data['search_query']
-    song_title = data['track_name']
-    ydl_opts = {
-        'quiet': True,
-        'extract_audio': True,
-        'format': '251/140/ba',
-        'outtmpl': f'static/songs/{song_title}.%(ext)s',
-        'external_downloader_args': ['-loglevel', 'panic'],
-        'progress_hooks': [progress_hook]
+    headers = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-GB,en;q=0.8',
+        'Cache-Control': 'no-cache',
+        'Origin': 'https://spotifydown.com',
+        'Pragma': 'no-cache',
+        'Referer': 'https://spotifydown.com/',
+        'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120", "Brave";v="120"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"Windows"',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'Sec-Gpc': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        if query=="sp":
-            ydl.download([f'ytsearch:"{squery} song"'])
-    input_file = glob.glob(f'static/songs/{song_title}*')[0]
-    fileext = input_file.split('.',-1)[1]
-    convertor = MDATA(input_file, data)
-    if fileext!= 'mp3' or fileext!= 'MP3':
-        for file in glob.glob(f'static/songs/{song_title}*'):
-            fileext = input_file.split('.',-1)[1]
-            if fileext== 'mp3':
-                convertor.export_mp3_with_metadata(input_file)
-                return input_file
-        eta = estimate_conversion_time(input_file)
-        socketio.emit('conversion', {'eta':eta})
-        convertor.convert_to_mp3()
-        return f'static/songs/{song_title}.mp3'
-    else:
-        convertor.export_mp3_with_metadata(input_file)
-        return input_file
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    new_url = data['link']
+    new_headers = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-GB,en;q=0.8',
+        'Cache-Control': 'no-cache',
+        'Origin': 'https://spotifydown.com',
+        'Pragma': 'no-cache',
+        'Referer': 'https://spotifydown.com/',
+        'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120", "Brave";v="120"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"Windows"',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'cross-site',
+        'Sec-Gpc': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    }
+
+    response = requests.get(new_url, headers=new_headers)
+
+    if response.ok:
+        content_disposition = response.headers.get('Content-Disposition')
+        filename = content_disposition.split('filename=')[1].replace('"', '') if content_disposition else 'output.mp3'
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        return filename
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0')
+    app.run(host='0.0.0.0')
     http_server = WSGIServer(('0.0.0.0'), app, handler_class=WebSocketHandler)
     http_server.serve_forever()
