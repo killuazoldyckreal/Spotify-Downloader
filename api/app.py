2d23ef00abbffaff5366d4988ab6a068c3b9379e
Killua Zoldyck <58257876+killuazoldyckreal@users.noreply.github.com>
2024-01-16 14:49:45 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -17,7 +17,22 @@ if not os.path.exists(cache_path):
     os.makedirs(cache_path)
 client_id=os.environ.get('CLIENT_ID')
 client_secret=os.environ.get('CLIENT_SECRET')
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+class CustomCacheHandler(CacheHandler):
+    def __init__(self, cache_path):
+        self.cache_path = cache_path
+
+    def get_cached_token(self):
+        cache_file = os.path.join(self.cache_path, 'token.txt')
+        if os.path.exists(cache_file):
+            with open(cache_file, 'r') as f:
+                token_info = f.read()
+                return eval(token_info) if token_info else None
+
+    def save_token_to_cache(self, token_info):
+        cache_file = os.path.join(self.cache_path, 'token.txt')
+        with open(cache_file, 'w') as f:
+            f.write(str(token_info))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret, cache_handler=CustomCacheHandler(cache_path)))
 
 @app.route('/.well-known/pki-validation/<filename>')
 def verification_file(filename):
