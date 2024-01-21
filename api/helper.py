f019c4a8732f369eee3ce153b498a7bc006beab9
Killua Zoldyck <58257876+killuazoldyckreal@users.noreply.github.com>
2024-01-21 17:12:13 +0530

    update

--- api/helper.py
+++ api/helper.py
@@ -138,7 +138,7 @@ def get_mp3(url):
         new_headers = {
             'Accept': '*/*',
             'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'en-GB,en;q=0.8',
+            'Accept-Language': 'en-GB,en;q=0.7',
             'Cache-Control': 'no-cache',
             'Origin': 'https://spotifydown.com',
             'Pragma': 'no-cache',
@@ -148,11 +148,10 @@ def get_mp3(url):
             'Sec-Ch-Ua-Platform': '"Windows"',
             'Sec-Fetch-Dest': 'empty',
             'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'cross-site',
+            'Sec-Fetch-Site': 'same-site',
             'Sec-Gpc': '1',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         }
-
         response = requests.get(new_url, headers=new_headers)
 
         if response.ok:
