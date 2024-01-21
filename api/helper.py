f5e27ae45479f5cf68713c966f685c9b88fe5b98
Killua Zoldyck <58257876+killuazoldyckreal@users.noreply.github.com>
2024-01-21 17:14:05 +0530

    update

--- api/helper.py
+++ api/helper.py
@@ -148,7 +148,7 @@ def get_mp3(url):
             'Sec-Ch-Ua-Platform': '"Windows"',
             'Sec-Fetch-Dest': 'empty',
             'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-site',
+            'Sec-Fetch-Site': 'cross-site',
             'Sec-Gpc': '1',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         }
