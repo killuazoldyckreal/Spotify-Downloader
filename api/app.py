1a7afc40d4778e3d386bafd7dc3a4716d315a2f5
Killua Zoldyck <58257876+killuazoldyckreal@users.noreply.github.com>
2024-01-23 13:11:41 +0530

    update

--- api/app.py
+++ api/app.py
@@ -18,7 +18,6 @@ client_id=os.environ.get('CLIENT_ID')
 client_secret=os.environ.get('CLIENT_SECRET')
 limiter = Limiter(
     app,
-    key_func=get_remote_address,
     storage_uri='memory://'
 )
 
