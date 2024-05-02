d708bcac9bbd0080aef97acd1c14bfca8cb593ad
Killua Zoldyck <58257876+killuazoldyckreal@users.noreply.github.com>
2024-05-02 14:10:56 +0530

    Update helper.py

--- api/helper.py
+++ api/helper.py
@@ -10,9 +10,11 @@ from dotenv import load_dotenv
 import logging
 
 load_dotenv()
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-console = logging.StreamHandler()
-console.setLevel(logging.ERROR)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', 
+                    handlers=[console], 
+                    datefmt='%Y-%m-%d %H:%M:%S', 
+                    exc_info=True)
+
 api_key=os.getenv('API_KEY')
 ACCESS_KEY = os.getenv('DROPBOX_KEY')
 ACCESS_SECRET = os.getenv('DROPBOX_SECRET')
@@ -64,69 +66,81 @@ def get_song_metadata(track_info, sp):
     return data
 
 def search_track(track_name, artist_name):
-    base_url = "https://api.musixmatch.com/ws/1.1/"
-    endpoint = "track.search"
-
-    params = {
-        "q_track": track_name,
-        "q_artist": artist_name,
-        "apikey": api_key,
-    }
-
-    response = requests.get(base_url + endpoint, params=params)
-    data = response.json()
-
-    if response.status_code == 200:
-        track_list = data["message"]["body"]["track_list"]
-        if track_list:
-            track_id = track_list[0]["track"]["track_id"]
-            return track_id
+    try:
+        base_url = "https://api.musixmatch.com/ws/1.1/"
+        endpoint = "track.search"
+    
+        params = {
+            "q_track": track_name,
+            "q_artist": artist_name,
+            "apikey": api_key,
+        }
+    
+        response = requests.get(base_url + endpoint, params=params)
+        data = response.json()
+    
+        if response.status_code == 200:
+            track_list = data["message"]["body"]["track_list"]
+            if track_list:
+                track_id = track_list[0]["track"]["track_id"]
+                return track_id
+            else:
+                return None
         else:
             return None
-    else:
+    except Exception as e:
+        logging.error("An error occurred: %s", e, exc_info=True)
         return None
 
 def get_lyrics(track_id):
-    base_url = "https://api.musixmatch.com/ws/1.1/"
-    endpoint = "track.lyrics.get"
-
-    params = {
-        "track_id": track_id,
-        "apikey": api_key,
-    }
-
-    response = requests.get(base_url + endpoint, params=params)
-    data = response.json()
-
-    if response.status_code == 200:
-        if data["message"]["body"]!=[]:
-            lyrics = data["message"]["body"]["lyrics"]["lyrics_body"]
-            return lyrics
-        return None
-    else:
+    try:
+        base_url = "https://api.musixmatch.com/ws/1.1/"
+        endpoint = "track.lyrics.get"
+    
+        params = {
+            "track_id": track_id,
+            "apikey": api_key,
+        }
+    
+        response = requests.get(base_url + endpoint, params=params)
+        data = response.json()
+    
+        if response.status_code == 200:
+            if data["message"]["body"]!=[]:
+                lyrics = data["message"]["body"]["lyrics"]["lyrics_body"]
+                return lyrics
+            return None
+        else:
+            return None
+    except Exception as e:
+        logging.error("An error occurred: %s", e, exc_info=True)
         return None
 
 def add_mdata(audio_file,metadata):
-    audio_file.seek(0)
-    tags = ID3()
-    cover_image_data = BytesIO(requests.get(metadata['cover_art_url']).content)
-    tags['TIT2'] = TIT2(encoding=3, text=metadata['track_name'])
-    tags['TPE1'] = TPE1(encoding=3, text=metadata['artists'])
-    tags['TALB'] = TALB(encoding=3, text=metadata['album_name'])
-    tags['TDRC'] = TDRC(encoding=3, text=metadata['release_date'])
-    tags['TCON'] = TCON(encoding=3, text=metadata['genres'])
-    if 'album_artists' in metadata:
-        tags['TPE2'] = TPE2(encoding=3, text=metadata['album_artists'])
-    tags['APIC'] = APIC(
-        encoding=0,
-        mime='image/jpeg',
-        type=3,
-        desc=u'Cover',
-        data=cover_image_data.getvalue()
-    )
-    tags.save(audio_file)
-    audio_file.seek(0)
-    return audio_file
+    try:
+        audio_file.seek(0)
+        tags = ID3()
+        cover_image_data = BytesIO(requests.get(metadata['cover_art_url']).content)
+        tags['TIT2'] = TIT2(encoding=3, text=metadata['track_name'])
+        tags['TPE1'] = TPE1(encoding=3, text=metadata['artists'])
+        tags['TALB'] = TALB(encoding=3, text=metadata['album_name'])
+        tags['TDRC'] = TDRC(encoding=3, text=metadata['release_date'])
+        tags['TCON'] = TCON(encoding=3, text=metadata['genres'])
+        if 'album_artists' in metadata:
+            tags['TPE2'] = TPE2(encoding=3, text=metadata['album_artists'])
+        tags['APIC'] = APIC(
+            encoding=0,
+            mime='image/jpeg',
+            type=3,
+            desc=u'Cover',
+            data=cover_image_data.getvalue()
+        )
+        tags.save(audio_file)
+        audio_file.seek(0)
+        return audio_file
+    except Exception as e:
+        logging.error("An error occurred: %s", e, exc_info=True)
+        return None
 
 def format_duration(milliseconds):
     seconds = milliseconds // 1000
@@ -143,60 +157,70 @@ def format_duration(milliseconds):
     return formatted_duration.strip()
 
 def upload_file(f, dropbox_path):
-    dbx = dropbox.Dropbox(app_key = ACCESS_KEY, app_secret = ACCESS_SECRET, oauth2_refresh_token = ACCESS_TOKEN)
-    response = dbx.files_upload(f.read(), dropbox_path, autorename=True)
-    shared_link_metadata = dbx.sharing_create_shared_link(path=response.path_display)
-    direct_link = shared_link_metadata.url.replace('&dl=0', '&dl=1')
-    direct_link2 = direct_link.replace('https://www.dropbox.com', 'https://dl.dropboxusercontent.com')
-    return direct_link, direct_link2
+    try:
+        dbx = dropbox.Dropbox(app_key = ACCESS_KEY, app_secret = ACCESS_SECRET, oauth2_refresh_token = ACCESS_TOKEN)
+        response = dbx.files_upload(f.read(), dropbox_path, autorename=True)
+        shared_link_metadata = dbx.sharing_create_shared_link(path=response.path_display)
+        direct_link = shared_link_metadata.url.replace('&dl=0', '&dl=1')
+        direct_link2 = direct_link.replace('https://www.dropbox.com', 'https://dl.dropboxusercontent.com')
+        return direct_link, direct_link2
+    except Exception as e:
+        logging.error("An error occurred: %s", e, exc_info=True)
 
 def delete_file(dropbox_path):
-    dbx = dropbox.Dropbox(app_key = ACCESS_KEY, app_secret = ACCESS_SECRET, oauth2_refresh_token = ACCESS_TOKEN)
-    dbx.files_delete(dropbox_path)
+    try:
+        dbx = dropbox.Dropbox(app_key = ACCESS_KEY, app_secret = ACCESS_SECRET, oauth2_refresh_token = ACCESS_TOKEN)
+        dbx.files_delete(dropbox_path)
+    except Exception as e:
+        logging.error("An error occurred: %s", e, exc_info=True)
     return
 
 def get_mp3(url):
-    response = requests.get(url)
-    if response.ok:
-        data = response.json()
-        gid = data["result"]["gid"]
-        tid = data["result"]["id"]
-        filename = data["result"]["name"]+".mp3"
-        url2 = f"https://api.fabdl.com/spotify/mp3-convert-task/{gid}/{tid}"
-        response2 = requests.get(url2)
-        if response2.ok:
-            data2 = response2.json()
-            statusid = data2["result"]["status"]
-            if statusid!=3:
-                headers = {
-                    "Accept": "*/*",
-                    "Accept-Language": "en-US,en;q=0.5",
-                    "Accept-Encoding": "gzip",
-                    "Referer": "https://spotifydown.com/",
-                    "Origin": "https://spotifydown.com"
-                }
-                baseurl = "https://api.spotifydown.com/download/"+tid
-                response3 = requests.get(baseurl, headers = headers)
+    try:
+        response = requests.get(url)
+        if response.ok:
+            data = response.json()
+            gid = data["result"]["gid"]
+            tid = data["result"]["id"]
+            filename = data["result"]["name"]+".mp3"
+            url2 = f"https://api.fabdl.com/spotify/mp3-convert-task/{gid}/{tid}"
+            response2 = requests.get(url2)
+            if response2.ok:
+                data2 = response2.json()
+                statusid = data2["result"]["status"]
+                if statusid!=3:
+                    headers = {
+                        "Accept": "*/*",
+                        "Accept-Language": "en-US,en;q=0.5",
+                        "Accept-Encoding": "gzip",
+                        "Referer": "https://spotifydown.com/",
+                        "Origin": "https://spotifydown.com"
+                    }
+                    baseurl = "https://api.spotifydown.com/download/"+tid
+                    response3 = requests.get(baseurl, headers = headers)
+                    if response3.ok:
+                        try:
+                            data = response3.json()
+                        except requests.exceptions.JSONDecodeError:
+                            return None, None
+                        if data["success"]:
+                            download_url = data["link"]
+                            response4 = requests.get(download_url)
+                            if response4.ok:
+                                audiobytes = response4.content
+                                return audiobytes, filename
+                    return None, None
+                download_url = "https://api.fabdl.com" + data2["result"]["download_url"]
+                response3 = requests.get(download_url)
                 if response3.ok:
-                    try:
-                        data = response3.json()
-                    except requests.exceptions.JSONDecodeError:
-                        return None, None
-                    if data["success"]:
-                        download_url = data["link"]
-                        response4 = requests.get(download_url)
-                        if response4.ok:
-                            audiobytes = response4.content
-                            return audiobytes, filename
-                return None, None
-            download_url = "https://api.fabdl.com" + data2["result"]["download_url"]
-            response3 = requests.get(download_url)
-            if response3.ok:
-                audiobytes = response3.content
-                return audiobytes, filename
+                    audiobytes = response3.content
+                    return audiobytes, filename
+                else:
+                    return None, None
             else:
                 return None, None
         else:
             return None, None
-    else:
+    except Exception as e:
+        logging.error("An error occurred: %s", e, exc_info=True)
         return None, None
